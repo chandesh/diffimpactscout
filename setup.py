ba7281266890724d78c07a7ea68224844c78a658
chandesh <chandesh@users.noreply.github.com>
2026-09-13 19:04:24 +0530

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with io.open("README.md", encoding="utf-8") as fh:
 
 setup(
     name="diffimpactscout",
-    version="0.1.0",
+    version="0.2.0",
     description="Incremental pre-push guard and AST blast-radius impact analyzer for git repositories.",
     long_description=long_description,
     long_description_content_type="text/markdown",
--- src/diffimpactscout/__init__.py
+++ src/diffimpactscout/__init__.py
@@ -1,3 +1,3 @@
 """diffimpactscout: incremental pre-push guard and AST blast-radius impact analyzer."""
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
